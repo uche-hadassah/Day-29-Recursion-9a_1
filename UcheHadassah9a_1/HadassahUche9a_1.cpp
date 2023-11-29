29983d221c04ff7e2205695709017f0cb3a86569
Hadassah Uche <uhadassah1@gmail.com>
2023-11-29 11:26:06 +0200

    Added the fibonacci function we did in class

--- UcheHadassah9a_1/HadassahUche9a_1.cpp
+++ UcheHadassah9a_1/HadassahUche9a_1.cpp
@@ -1,3 +1,11 @@
 /*The recursive solution to the problem of finding the Fibonacci number we saw in 
 class, is inefficient. Why? Write a non-recursive Fibonacci function. (You can 
 provide the answer to the question in a commented part of your code).*/
+/*long fibonacci( long n )
+{
+if ( n == 0 || n == 1 ) // base case
+ return n;
+ else 
+ return fibonacci( n - 1 ) + fibonacci( n – 2 );
+}
+*/
